c59c08b259f9925462015022f8c5fd3a5c03e0e7
thompsa2 <aren.thompson@gmail.com>
2025-07-13 21:02:57 -0500

    add FPS limiter

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0
 
 def main():
     while True:
@@ -13,9 +15,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        pygame.Surface.fill(screen, (0,0,0))
+        screen.fill("black")
         pygame.display.flip()
-    
+        dt = clock.tick(60) / 1000
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
